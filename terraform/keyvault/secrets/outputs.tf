7850c777a10a4aeadb2544c391e05d2c5964cfb1
minion <55378929+nguyen-anh-dat@users.noreply.github.com>
2025-07-21 15:02:37 +0700

    Update outputs.tf

--- terraform/keyvault/secrets/outputs.tf
+++ terraform/keyvault/secrets/outputs.tf
@@ -1,1 +1,9 @@
+output "key_vault_id" {
+  description = "The ID of the Key Vault."
+  value       = azurerm_key_vault.kv-sea-dat-dev.id
+}
 
+output "key_vault_uri" {
+  description = "The URI of the Key Vault."
+  value       = azurerm_key_vault.kv-sea-dat-dev.vault_uri
+}
